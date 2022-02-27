176762539166ae9d40602f2339b090a6ef80b325
Howard Mall <howardmall+github@howardmall.net>
2022-02-27 16:53:40 -0500

    + Red gutter guide at the end to know when it is shifts + Added a white mask to show when you are at the end of the line before   a new line. + Corrected a bunch of stuff having to do with scale and placement

--- www/js/writeblock.js
+++ www/js/writeblock.js
@@ -1,85 +1,54 @@
-// $("#board").redraw();
-
-
-function draw_path( dp )
-{
-	/*
-	if (writeblock && writeblock.getContext) {
-		wbctx = writeblock.getContext("2d");
-		// background
-		wbctx.fillStyle = "#F1F1F1";
-		wbctx.fillRect(0, 0, writeblock.width, writeblock.height);
-	}
-	*/
-	wbctx.strokeStyle = "green"
-	wbctx.lineWidth = 1.0;
-	wbctx.lineCap = "round";
-	wbctx.lineJoin = "round";
-	for( var i=0; i < dp.length-1; i++ )
-	{
-		var p = dp[i];
-		wbctx.beginPath();
-		wbctx.moveTo(p.x,p.y);
-		wbctx.lineTo(dp[i+1].x,dp[i+1].y)	
-		wbctx.stroke();
-		wbctx.closePath();
-	}
-	wbctx.strokeStyle = "red"
-	wbctx.lineWidth = 5.0;
-}
-
-var paths = [];
-var path = [];
-var gutter = 50;
-
-console.log("WINDOW: width = "+window.innerWidth+" height = "+window.innerHeight);
-
-var writeblock = document.getElementById("write-block");
-console.log("WRITEBLOCK: width = "+writeblock.width+" height = "+writeblock.height);
-writeblock.width = window.innerWidth-24;
-writeblock.height = 150; // writeblock.innerHeight; // window.innerHeight-24;
+var scale = 5.0;
+var zoom = 1.0;
+var boardscale = 1.0/scale;
+var bcsrx = 0;
+var bcsry = 0;
+var linespacing = 20;
+var gridsize = 20;
+var gutteroffset = 0;
+var leftgridoffset = gridsize/2;
+var gutter = 0;
+var endcap = 0;
+var spans = 0;
 
 var board = document.getElementById("board");
-console.log("BOARD: width = "+board.width+" height = "+board.height);
-board.width = window.innerWidth-24;
-board.height = window.innerHeight-174;
+var rect = board.getBoundingClientRect();
+board.width = window.innerWidth-2*rect.y;
+board.height = window.innerHeight*0.8;
+console.log("board("+rect.x+", "+rect.y+", "+board.width+","+board.height+")");
+var bctx = board.getContext("2d");
+bctx.strokeStyle = "blue"
+bctx.lineWidth = 1.0;
+bctx.lineCap = "round";
+bctx.lineJoin = "round";
 
-var zoom = 1.0
-var wbctx = null;
-var wbx, wby = 0;
-var bbx, bby = 0;
-	
-if (writeblock && writeblock.getContext) {
-	wbctx = writeblock.getContext("2d");
-	// background
-	// wbctx.fillStyle = "#F1F1F1";
-	// wbctx.fillRect(0, 0, writeblock.width, writeblock.height);
-}
+var writeblock = document.getElementById("writeblock");
+writeblock.width = (board.width)/scale
+writeblock.height = 40;
+var wbctx = writeblock.getContext("2d");
 wbctx.strokeStyle = "red"
-wbctx.lineWidth = 5.0;
+wbctx.lineWidth = 1.0;
 wbctx.lineCap = "round";
 wbctx.lineJoin = "round";
 
-if (board && board.getContext) {
-	bbctx = board.getContext("2d");
-	// background
-	// bbctx.fillStyle = "#F1F1F1";
-	// bbctx.fillRect(0, 0, board.width, board.height);
-}
-bbctx.strokeStyle = "green"
-bbctx.lineWidth = 1.0;
-bbctx.lineCap = "round";
-bbctx.lineJoin = "round";
+console.log(writeblock.width);
 
-function sendArray( m )
+endcap = (writeblock.width-leftgridoffset) % gridsize;
+console.log("endcap = "+endcap);
+
+gutter = gutteroffset + endcap;
+console.log("gutter = "+gutter);
+formatgutterguide();
+console.log(gutter);
+
+// var boardendwidth = (writeblock.width - (board.width % writeblock.width));
+$("#mask").css({top: board.height+25});
+
+function formatgutterguide()
 {
-	socket.emit("message",m)
+	$("#gutter").css({width: gutter*scale, top: board.height+25, left: scale*((writeblock.width)-gutter)+gutteroffset+leftgridoffset});
 }
 
-bs = 0.4;
-bbcsrx = 0;
-bbcsry = 0;
-linespacing = 36;
 function draw_start(evt)
 {
 		writeblock.style.cursor = 'default';
@@ -102,24 +71,11 @@ function draw_start(evt)
 		writeblock.addEventListener("touchend", draw_end, false);
 }
 
-function carriagereturn()
-{
-	shift();
-	bbcsry = bbcsry + linespacing;
-	bbcsrx = 0;
-}
-
 function draw_move(evt)
 {
-	if (wbctx && bbctx) {
-		wbctx.beginPath();
-		bbctx.beginPath();
-		path.push({"x":x,"y":y})
-		wbctx.moveTo(x, y);
-		console.log(x*bs+bbcsrx);
-		if( (x*bs+bbcsrx) > (board.width - linespacing) )
-			carriagereturn()
-		bbctx.moveTo(x*bs+bbcsrx, y*bs+bbcsry);
+	if (wbctx && bctx) {
+		bctx.beginPath();
+		bctx.moveTo(x*boardscale+bcsrx, y*boardscale+bcsry);
 		newx = evt.offsetX ? evt.offsetX : (evt.pageX - evt.target.offsetLeft);
 		newy = evt.offsetY ? evt.offsetY : (evt.pageY - evt.target.offsetTop);
 		touches = evt.changedTouches;
@@ -129,58 +85,62 @@ function draw_move(evt)
 			newx = t.offsetX ? t.offsetX : (t.pageX - evt.target.offsetLeft);
 			newy = t.offsetY ? t.offsetY : (t.pageY - evt.target.offsetTop);
 		}
-		// console.log(evt.offsetX+" "+evt.pageX+" "+evt.target.offsetLeft);
 		x = 1/zoom*newx;
 		y = 1/zoom*newy;
-		wbctx.lineTo(x, y);
-		bbctx.lineTo(x*bs+bbcsrx, y*bs+bbcsry);
-		path.push({x,y})
-		wbctx.stroke();
-		bbctx.stroke();
-		wbctx.closePath();
-		bbctx.stroke();
-		document.getElementById("write-block").style.cursor = 'default !important';
+		bctx.lineTo(x*boardscale+bcsrx, y*boardscale+bcsry);
+		bctx.stroke();
+		bctx.stroke();
+		document.getElementById("writeblock").style.cursor = 'default !important';
+		wbctx.drawImage(board,bcsrx,bcsry,writeblock.width,writeblock.height,0,0,writeblock.width,writeblock.height);
 	}
-	// sendArray(path)
-	path = []
 }
 
 function draw_end(evt)
 {
 	writeblock.removeEventListener("mousemove", draw_move, false);
 	writeblock.removeEventListener("touchmove", draw_move, false);
-	// console.log("x = "+x)
-	if( x > writeblock.width-gutter)
+	if( x/scale > writeblock.width-gutter)
 	{
-		shift();
-		bbcsrx += (writeblock.width-gutter) * bs;
+		bcsrx += writeblock.width-leftgridoffset-endcap;
+		console.log("bcsrx = "+bcsrx);
+		console.log("board.width = "+board.width);
+		if( bcsrx > (board.width - 15) )
+		{
+			carriage_return();
+		}
+		else
+		{
+			shift_writing();
+			console.log("board.width-writeblock.width = "+(board.width-writeblock.width));
+			if( bcsrx > board.width-writeblock.width )
+			{
+				gutter = bcsrx - (board.width-writeblock.width)+leftgridoffset;
+				console.log("gutter = "+gutter);
+				formatgutterguide();
+				$("#mask").css({visibility:"visible", width: (gutter-leftgridoffset)*scale});
+			}
+		}
 	}
 }
-
-function shift()
+function shift_writing()
 {
-	var imageData = wbctx.getImageData(writeblock.width-gutter,0,writeblock.width,writeblock.height);
+	var imageData = wbctx.getImageData(writeblock.width-leftgridoffset-endcap,0,leftgridoffset+endcap,writeblock.height);
 	wbctx.clearRect(0,0,writeblock.width,writeblock.height);
 	wbctx.putImageData(imageData,0,0);
+	wbctx.drawImage(board,bcsrx,bcsry,writeblock.width,writeblock.height,0,0,writeblock.width,writeblock.height);
 }
+
+function carriage_return()
+{
+	bcsry = bcsry + linespacing;
+	bcsrx = 0;
+	gutter = gutteroffset + endcap;
+	formatgutterguide();
+	shift_writing();
+	$("#mask").css({visibility:"hidden"});
+}
+
+// wbctx.drawImage(board,0,0,scale*writeblock.width,scale*scale*writeblock.height);
 
 writeblock.onmousedown = draw_start;
 writeblock.addEventListener("touchstart",draw_start,false);
-
-$("#return-button").mouseup(function(event)
-{
-	carriagereturn();
-});
-	
-board.onmousewheel = function (event){
-	var wheel = event.wheelDelta/33;//n or -n
-	if(wheel < 0)
-	{
-		zoom = zoom -0.1;
-	}
-	else
-	{
-		zoom = zoom + 0.1;
-	}
-	document.body.style.zoom=(zoom*100)+"%";
-}
